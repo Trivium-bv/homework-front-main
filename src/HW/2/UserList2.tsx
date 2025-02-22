3c2c96b12d5f53790555045ac2cd8c529a08084e
Trivium-bv <trivium.bv@gmail.com>
2025-02-22 21:59:51 +0300

    HW2

---
+++ src/HW/2/UserList2.tsx
@@ -0,0 +1,23 @@
+import {CurrentUser} from './CurrentUser';
+import {UsersObjectType, UserType} from './HW2';
+
+type UserList2PropsType = {
+    users: UsersObjectType // пропиши типизацию
+    filterUsers: (myFriends: UserType[]) => void // пропиши типизацию
+};
+
+export const UserList2 = (props: UserList2PropsType) => {
+    return (
+        <div id={'hw02-users'}>
+            <h2>User List:</h2>
+            <button id={'hw02-filter-button'} onClick={() => (props.filterUsers(props.users.myFriends))}>SHOW ME FRIENDS
+                FROM LA
+            </button>
+            <ul>
+                {props.users.myFriends.map((user, index) => (
+                    < CurrentUser key={index} user={user}/>
+                ))}
+            </ul>
+        </div>
+    );
+};
