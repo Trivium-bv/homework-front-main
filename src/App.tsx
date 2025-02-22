a730477dd76fee5e597d2a653f1a5872bf4c94b1
Trivium-bv <trivium.bv@gmail.com>
2025-02-22 21:59:38 +0300

    HW2

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css';
-import { HW1 } from './HW/1/HW1';
+// import {HW1} from './HW/1/HW1';
+import {HW2} from './HW/2/HW2.tsx';
 
 function App() {
-  return (
-    <div className="App">
-      <HW1 />
-    </div>
-  );
+    return (
+        <div className="App">
+            {/*<HW1/>*/}
+            <HW2/>
+        </div>
+    );
 }
 
 export default App;
